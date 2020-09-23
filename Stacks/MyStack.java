f51242f53df2a1db60695d95f77429120c581255
Michael Ades <59174641+MichaelA-US@users.noreply.github.com>
2020-09-23 06:30:03 -0500

    Update MyStack.java

--- Stacks/MyStack.java
+++ Stacks/MyStack.java
@@ -1,44 +1,62 @@
 import java.util.EmptyStackException;
-
+/**
+ * This class implements multiple methods to recreate a replica of the Stack class
+ */
 public class MyStack
 {
     private Integer[] stack;
     private int size;
-
+    /**
+     * Default constructor of the class
+     */
     public MyStack()
     {
         size = 0;
     }
-
+    /**
+     * Second constructor that
+     * initalized the variables and size
+     */
     public MyStack(int initCap)
     {
         this();
         stack = new Integer[initCap];
         size = initCap;
     }
-
+    /**
+     * Finds if the stack doesn't have any objects in it
+     * @return a true or false - true being if it is empty, false if not
+     */
     public boolean isEmpty()
     {
-        if(stack.length == 0)
+        if(stack.length == 0) //Checks if stack is empty
         {
             return true;
         }
         return false;
     }
-
+    /**
+     * Finds the top value on the stack
+     * Throws an exception if not possible
+     * @return the value on the top
+     */
     public Integer Peek()
     {
-        if(isEmpty())
+        if(isEmpty()) //If no elements throw exception
         {
             throw new EmptyStackException();
         }
         return stack[size - 1];
 
     }
-
+    /**
+     * Removes the top value and returns it; throws an exception
+     * if not possible
+     * @return the value removed
+     */
     public Integer pop()
     {
-        if(isEmpty())
+        if(isEmpty()) //If no elements throw exception
         {
             throw new EmptyStackException();
         }
@@ -46,37 +64,48 @@ public class MyStack
         size--;
         return temp;
     }
-
+    /**
+     * Adds an item to the stack
+     * @param the item to add
+     */
     public void push(Integer item)
     {
-        if(size == stack.length)
+        if(size == stack.length) //Checks if stack is at max size
         {
             doubleCapacity();
         }
         stack[size] = item;
         size++;
     }
-
+    /**
+     * Doubles the capacity of the array
+     * to ensure the stack has enough space when adding items
+     */
     public void doubleCapacity()
     {
-        Integer[] tempStack = new Integer[stack.length * 2];
+        Integer[] tempStack = new Integer[stack.length * 2]; //Double the stack
         for(int i = 0; i < stack.length; i++)
         {
-            tempStack[i] = stack[i];
+            tempStack[i] = stack[i]; //Add values
         }
-        stack = tempStack;
+        stack = tempStack; 
     }
+    /**
+     * Convert the stack to a string in a
+     * 'stack' like way
+     * @return the stack
+     */
     @Override
     public String toString()
     {
         if(isEmpty())
         {
-            return "[]";
+            return "[]"; 
         }
         String s = "[";
         for(Integer i: stack)
         {
-            if(i == null)
+            if(i == null) //If the element is null don't add the item to string
             {
                 break;
             }
