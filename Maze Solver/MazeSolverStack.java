8f8f1596b08d17cbcd1992147f3432a62cd3b5e9
Michael Ades <59174641+MichaelA-US@users.noreply.github.com>
2020-09-27 23:21:47 -0500

    Update MazeSolverStack.java

--- Maze Solver/MazeSolverStack.java
+++ Maze Solver/MazeSolverStack.java
@@ -1,5 +1,60 @@
-
-public class MazeSolverStack extends MazeSolver 
+/**
+ * This class implements the abstract class MazeSolver and
+ * overrides the abstract methods to implement basic
+ * operations for a stack using the MyStack class
+ *
+ */
+public class MazeSolverStack extends MazeSolver
 {
+    private MyStack workList;
 
+    /**
+     * Basic constructor that calls upon the super maze
+     * @param maze
+     */
+    public MazeSolverStack(Maze maze)
+    {
+        super(maze);
+    }
+
+    /**
+     * Makes the stack empty
+     */
+    @Override
+    void makeEmpty()
+    {
+        workList = new MyStack();
+    }
+
+    /**
+     * Checks if the stack is empty
+     * @return true or false depending on if the stack is empty or not
+     */
+    @Override
+    boolean isEmpty()
+    {
+        return workList.isEmpty();
+    }
+
+    /**
+     * Push the square onto the stack and print
+     * @param s
+     */
+    @Override
+    public void add(Square s)
+    {
+        workList.push(s);
+        //System.out.println(workList); Test to make sure it's working
+    }
+
+    /**
+     * Returns the next square using pop
+     * @return
+     */
+    @Override
+    Square next()
+    {
+
+        return workList.pop();
+    }
 }
