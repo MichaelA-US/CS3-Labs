44b1fab6d1153a124045ae94a76bb9a9faa69a78
Michael Ades <59174641+MichaelA-US@users.noreply.github.com>
2020-10-26 09:55:32 -0500

    Update MyBST.java

--- MyBinarySearchTree/MyBST.java
+++ MyBinarySearchTree/MyBST.java
@@ -4,6 +4,7 @@
  * @author Am10073106
  *
  */
+import java.util.*;
 public class MyBST
 {
 	/**
@@ -49,9 +50,9 @@ public class MyBST
 	 */
 	public void insert(Integer n)
 	{
-            if(root == null)
+        if(root == null)
 	    {
-                root = new BSTNode(n);
+            root = new BSTNode(n);
 		    return;
 	    }
 		insert(n, root);
@@ -60,23 +61,27 @@ public class MyBST
 	{
 		if(n < root.val)
 		{
-	           if (root.left == null)
+	        if (root.left == null)
 		    {
-                         root.left = new BSTNode(n);
-                         return;
-                    }
-                    insert(n, node.left);
-		}
-		else
-		{
-                    if (root.right == null)
-		    {
-                         root.right = new BSTNode(n);
-                         return;
-                    }
-                    insert(n, node.right);
+               root.left = new BSTNode(n);
+               return;
+            }
+            	insert(n, root.left);
+			}
+			else
+			{
+				if(root.val == n)
+				{
+					return;
+				}
+				if (root.right == null)
+				{
+                   root.right = new BSTNode(n);
+                   return;
+                 }
+                 insert(n, root.right);
+			}
 			
-		}
 	}
 	/**
 	 * Find a node with the same value given in the tree
@@ -86,19 +91,19 @@ public class MyBST
 	public boolean contains(Integer n) 
 	{
         return contains(root, n);
-        }
-        private boolean contains(BSTNode root, Integer n) 
+    }
+    private boolean contains(BSTNode root, Integer n) 
 	{
         if (root == null) 
-	{
-	 return false;
-	}
-        if (root.val.equals(n))
-	{
-        return true;
-	}
-        return contains(root.left, n) || contains(root.right, n);
+        {
+        	return false;
         }
+        if (root.val.equals(n))
+        {
+        	return true;
+        }
+       	return contains(root.left, n) || contains(root.right, n);
+    }
 	/**
 	 * Returns the largest value in the tree,
 	 * or null if the tree is empty
@@ -106,16 +111,40 @@ public class MyBST
 	 */
 	public Integer getMax()
 	{
-
+		if(root == null)
+		{
+			return null;
+		}
+		return getMax(root);
+	}
+	private Integer getMax(BSTNode root)
+	{
+		if(root.right == null)
+		{
+			return root.val;
+		}
+		return getMax(root.right);
 	}
 	/**
 	 * Returns the smallest value in the tree,
 	 * or null if the tree is empty
 	 * @return
 	 */
-	public Integer  getMin()
+	public Integer getMin()
 	{
-
+		if(root == null)
+		{
+			return null;
+		}
+		return getMin(root);
+	}
+	public Integer getMin(BSTNode root)
+	{
+		if(root.left == null)
+		{
+			return root.val;
+		}
+		return getMin(root.left);
 	}
 	/**
 	 * Delete a node in the tree with value n. Does nothing if n
@@ -124,15 +153,65 @@ public class MyBST
 	 */
 	public void delete(Integer n)
 	{
+		delete(n, root);
+	}
+	private void delete(Integer n, BSTNode root)
+	{
+		if(root.val.equals(n))
+		{
+			if(root.right == null && root.left == null)
+			{
+				root.val = 0;
+			}
+		}
 		
 	}
 	public void inOrder()
 	{
-		
+		System.out.print("[");
+		inOrder(root);
+		System.out.print("]");
+	}
+	private void inOrder(BSTNode root)
+	{
+		if(root == null)
+		{
+			return;
+		}
+		inOrder(root.left);
+		if(root.val < (getMax()))
+		{
+			System.out.print(root.val + ", ");
+		}
+		else
+		{
+			System.out.print(root.val);
+		}
+		inOrder(root.right);
 	}
 	public void print()
 	{
-		System.out.println(root);
+		print(root, 0);
 	}
-	
+	private void print(BSTNode root, int level)
+	{
+		
+		if(root == null)
+		{
+			return;
+		}
+		print(root.right, level +1);
+		System.out.println(helpIndent(level) + root.val);
+		print(root.left, level +1);
+	}
+	public String helpIndent(int level)
+	{
+		int tab = 4;
+		StringBuilder storeSpaces = new StringBuilder();
+		for(int i = 0; i < level * tab; i++)
+		{
+			storeSpaces.append(" ");
+		}
+		return storeSpaces.toString();
+	}
 }
