44d69dbe51449fe4d5bcc72b4a2ab1df0297c57e
Michael Ades <59174641+MichaelA-US@users.noreply.github.com>
2020-12-01 12:49:40 -0600

    Update BoggleSolver.java

--- Labs/src/BoggleSolver.java
+++ Labs/src/BoggleSolver.java
@@ -1,12 +1,20 @@
-
 import java.io.*;
 import java.util.*;
-
+/**
+ * This class implements multiple methods to solve a boggleboard in a certain situation
+ * with a given dictionary
+ * @author Michael
+ *
+ */
 public class BoggleSolver 
 {
 	private Set<String> dictionary;
 	private Set<String> words;
-
+	/**
+	 * Basic constructor that reads in file of the dictionary and stores it into 
+	 * the dictionary hashset
+	 * @param dictionaryName - the name of dictionary to add
+	 */
 	public BoggleSolver(String dictionaryName) 
 	{
 		dictionary = new HashSet<String>();
@@ -23,7 +31,13 @@ public class BoggleSolver
 		{
 		}
 	}
-
+	/**
+	 * Valid word helper that loops through each index on the boggle board and recursively calls
+	 * the validHelper method to cget a set of all the valid words in the Boggle board (I also keep track of
+	 * the time it takes in seconds)
+	 * @param board - the Boggleboard
+	 * @return the hashset of the words
+	 */
 	public Iterable<String> getAllValidWords(BoggleBoard board) 
 	{
         final long S = System.nanoTime();
@@ -38,9 +52,20 @@ public class BoggleSolver
 		 System.out.println("Time: " + (E - S) / 1000000000 + "seconds");
 		return words;
 	}
-
+	/**
+	 * Valid helper methods that actually adds the words to set of words; does this by first making sure that
+	 * the word is greater than 2 and that dictionary actually contains the word, and then we get the letter from the board
+	 * and add QU to it if it starts with Q and then iteravely do this thorughout the boggleboard
+	 * @param board - the boggleboard
+	 * @param i - x index
+	 * @param j - y index
+	 * @param size - the size of the word
+	 * @param word - the word to append it to
+	 * @param v - keeps treacking of visiting the coordinate locations
+	 */
 	public void validHelper(BoggleBoard board, int i, int j, int size, String word, boolean[][] v)
 	{
+		String word2 = "";
 		if (size >= 3 && dictionary.contains(word))
 		{
 			words.add(word);
@@ -52,25 +77,29 @@ public class BoggleSolver
 		}
 		v[i][j] = true;
 		int add = 1;
-		if (addition(i, j, board).equals("Qu"))
+		if (board.getLetter(i, j) == 'Q')
+		{
 			add = 2;
+			word2 = "QU";
+		}
+		else
+		{
+			word2 = ""+board.getLetter(i, j);
+		}
 		for(int x = -1; x <= 1; x++)
 		{
 			for(int y = -1; y <= 1; y++)
 			{
-				validHelper(board, i + x, j + y, size + add, word + addition(i, j, board), v);
+				validHelper(board, i + x, j + y, size + add, word + word2, v);
 			}
 		}
 		v[i][j] = false;
 	}
-
-	public String addition(int i, int j, BoggleBoard board)
-	{
-		if (board.getLetter(i, j) == 'Q')
-			return "QU";
-		return board.getLetter(i, j) + "";
-	}
-
+	/**
+	 *Returns the score of the given word if it is in the dictionary, zero otherwise.
+	 * @param word - the word
+	 * @return score of word
+	 */
 	public int scoreOf(String word) {
 		if (word.length() <= 4)
 		{
@@ -94,6 +123,13 @@ public class BoggleSolver
 		}
 		return 0;
 	}
+	/**
+	 * Checks if the index given is not in the boundary constraints
+	 * @param i - x index
+	 * @param j - y index
+	 * @param board - boggleboard
+	 * @return true or false if its not in bounds or is
+	 */
 	public boolean notInBounds(int i, int j, BoggleBoard board)
 	{
 		return i < 0 || i >= board.rows() || j < 0 || j >= board.cols();
