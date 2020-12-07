6776b76d3aba4d0bf342e5889cf6d47b9784467e
Michael Ades <59174641+MichaelA-US@users.noreply.github.com>
2020-12-07 13:54:01 -0600

    Add files via upload

---
+++ Graphing/Vertex.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Vertex
+{
+	private int x, y, ID;
+	private List<Integer> edges;
+	private boolean visited;
+	private double distance;
+	public Vertex(int x, int y, int ID)
+	{
+		this.x = x;
+		this.y = y;
+		this.ID = ID;
+	}
+	public void eucDist(int x, int y)
+	{
+		
+	}
+	//@Override
+//	public int compareTo(Object o)
+//	{
+//		return 1;
+//	}
+	//@Override
+	public String toString()
+	{
+		return "";
+	}
+}
